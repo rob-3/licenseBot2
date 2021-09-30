281498c76c8708c14a0c3ca812756fa7ebec2208
Robert Boyd III <rob@robertcboyd.com>
2021-09-30 18:48:01 -0400

    Grab repo information from gh

--- index.mjs
+++ index.mjs
@@ -1,4 +1,5 @@
 import { Client, Intents } from "discord.js";
+import fetch from "node-fetch";
 
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
@@ -8,8 +9,13 @@ client.once("ready", () => {
   console.log("Bot is ready!");
 })
 
-client.on("messageCreate", (message) => {
-  console.log(message.content);
+client.on("messageCreate", async (message) => {
+  const regex = /https?:\/\/github.com\/([\d\w-\.]+\/[\d\w-\.]+)/;
+  const matches = message.content.match(regex);
+  if (matches !== null) {
+    const repo = await fetch("https://api.github.com/repos/" + matches[1]).then(b => b.json());
+    console.log(repo);
+  }
 })
 
 client.login("ODkzMjYzNTUyMjg5MzM3NDE1.YVY6gg.8_xV9PDlA_mqdTLD8z60vEO_1Sc");
