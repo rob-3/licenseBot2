35f2247fcdb9701f91d2e5b491dbc49efd60bef8
Robert Boyd III <rob@robertcboyd.com>
2021-09-30 18:36:07 -0400

    Initialize bot

---
+++ index.mjs
@@ -0,0 +1,11 @@
+import { Client, Intents } from "discord.js";
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+client.once("ready", () => {
+  console.log("Bot is ready!");
+})
+
+client.login("ODkzMjYzNTUyMjg5MzM3NDE1.YVY6gg.8_xV9PDlA_mqdTLD8z60vEO_1Sc");
